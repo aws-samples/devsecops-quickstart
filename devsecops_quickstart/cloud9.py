548975ad6d1a1f720fb546430528e37cbf01235a
Mahdi Ebrahimi <ebrmahdi@amazon.com>
2021-09-09 15:49:42 +0200

    added admin user for cloud9

--- devsecops_quickstart/cloud9.py
+++ devsecops_quickstart/cloud9.py
@@ -1,5 +1,7 @@
 import aws_cdk.aws_cloud9 as cloud9
 import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_iam as iam
+import aws_cdk.aws_secretsmanager as secretmanager
 import aws_cdk.core as cdk
 import aws_cdk.aws_codecommit as codecommit
 
@@ -19,18 +21,52 @@ class Cloud9Stack(cdk.Stack):
             value=repository.repository_clone_url_http,
         )
 
-        vpc = ec2.Vpc(self, "Cloud9-VPC", max_azs=3)
-        c9env = cloud9.Ec2Environment(
+        secret = secretmanager.Secret(
             self,
-            "Cloud9-Env",
-            vpc=vpc,
-            subnet_selection=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
-            cloned_repositories=[
-                cloud9.CloneRepository.from_code_commit(repository, "/"),
-            ],
+            id="cloud9_admin_password",
+            secret_name="cloud9_admin_password",
+            description="cloud9 admin password"
         )
 
-        cdk.CfnOutput(self, "Cloud9_IDE_URL", value=c9env.ide_url)
+        cloud9_admin_user = iam.User(
+            self,
+            id="cloud9_admin",
+            user_name="cloud9_admin",
+            password=secret.secret_value
+        )
+        cloud9_admin_user.add_managed_policy(
+            iam.ManagedPolicy.from_managed_policy_arn(
+                self,
+                id='AWSCloud9Administrator',
+                managed_policy_arn='arn:aws:iam::aws:policy/AWSCloud9Administrator'
+            )
+        )
+        cloud9_admin_user.add_managed_policy(
+            iam.ManagedPolicy.from_managed_policy_arn(
+                self,
+                id='AWSCodeCommitFullAccess',
+                managed_policy_arn='arn:aws:iam::aws:policy/AWSCodeCommitFullAccess'
+            )
+        )
+        cdk.CfnOutput(self, "Cloud9_Admin_User", value=cloud9_admin_user.user_arn)
+        cdk.CfnOutput(self, "Cloud9_admin_Password_Secret", value=secret.secret_arn)
+
+        vpc = ec2.Vpc(self, "Cloud9-VPC", max_azs=3)
+
+        cloud9_environment = cloud9.CfnEnvironmentEC2(
+            self,
+            general_config["repository_name"],
+            instance_type="t2.micro",
+            owner_arn=cloud9_admin_user.user_arn,
+            subnet_id=vpc.public_subnets[0].subnet_id,
+            repositories=[cloud9.CfnEnvironmentEC2.RepositoryProperty(
+                repository_url=repository.repository_clone_url_http,
+                path_component=f"/{general_config['repository_name']}"
+            )]
+        )
+
+        ide_url = f"https://{general_config['toolchain_region']}.console.aws.amazon.com/cloud9/ide/{cloud9_environment.ref}"
+        cdk.CfnOutput(self, "Cloud9_IDE_URL", value=ide_url)
 
 
 class Cloud9Stage(cdk.Stage):
